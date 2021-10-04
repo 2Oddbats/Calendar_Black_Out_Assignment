3a5ade92e5e1b3b781a7a3c2feeec57249590a86
Isaiah Skelton <89801103+2Oddbats@users.noreply.github.com>
2021-10-04 13:17:45 -0500

    Blacking out dates on calendars

---
+++ Calendar_Black_Out_Assignment/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar_Black_Out_Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Instantiate Two Date Time Variable
+
+        public DateTime myFirstDate, mySecondDate;
+
+       
+
+
+        // Get First Date
+
+        public void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            myFirstDate = myCalendar.SelectedDate.Value;
+        
+        } 
+
+        // Get Second Date
+
+        public void mySecondCalendar_SelectedDatesChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+            mySecondDate = mySecondCalendar.SelectedDate.Value;
+
+        }
+
+        // Call Black Out Method
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SetBlackOutDates(myFirstDate, mySecondDate);
+
+        }
+
+        private void SetBlackOutDates(DateTime myFirstDate, DateTime mySecondDate)
+        {
+            blackOutCalendar.BlackoutDates.Add(new CalendarDateRange(myFirstDate, mySecondDate));
+        }
+    }
+   
+}
